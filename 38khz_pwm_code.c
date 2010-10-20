573598a927d0d3c4405499de5c490de4a16b627c
Mike Pountney <Mike.Pountney@gmail.com>
2010-10-20 19:33:20 +0100

    adding matts 38khz_pwm_code.c

---
+++ 38khz_pwm_code.c
@@ -0,0 +1,71 @@
+ // the below should generate a 38k ir signal. I have more somewhere with the ir pulsing separate [so it works summink like sendIr(HIGH|LOW); // pulse @ 38k for 6 cycles [low] or 12 cycls [high]]. Just loops through a byte sending out each bit
+
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <avr/sleep.h>
+#include <avr/pgmspace.h>
+
+
+#define redMask  0b00000010;
+#define grnMask  0b00010000;
+#define bluMask  0b00000100;
+#define irIn     0b00000000;
+#define irOut    0b00000001;
+
+void delay_ten_us(unsigned long int us) {
+  unsigned long int count;
+  const unsigned long int DelayCount=6;
+
+  while (us != 0) {
+    for (count=0; count <= DelayCount; count++) {PINB |= 0b00100000;};
+    us--;
+  }
+}
+
+void delay_x_us(unsigned long int x) {
+  unsigned long int count;
+  const unsigned long int DelayCount=0;
+
+  while (x != 0) {
+    for (count=0; count <= DelayCount; count++) {PINB |= 0b00100000;};
+    x--;
+  }
+}
+
+
+// Little start up pattern
+void startUp1() {
+    for (int i = 0; i<15; i++) {
+        PORTB ^= redMask;
+        delay_ten_us(1600-(i*100));
+        PORTB ^= redMask;
+        delay_ten_us(1600-(i*100));
+        PORTB ^= grnMask;
+        delay_ten_us(1600-(i*100));
+        PORTB ^= grnMask;
+        delay_ten_us(1600-(i*100));
+        PORTB ^= bluMask;
+        delay_ten_us(1600-(i*100));
+        PORTB ^= bluMask;
+        delay_ten_us(1600-(i*100));
+    }
+}
+
+
+int main(void) {
+    TCCR0A = 0;
+    TCCR0B = 0;
+    DDRB = 0b00010111;
+    PORTB = 0xff; // write all outputs high & internal resistors on inputs (turns led's off)
+    
+    startUp1();
+    
+    while (1==1) {
+        
+        PORTB ^= irOut;
+        PORTB ^= redMask;
+        delay_x_us(26); // 17 works
+        
+    }
+}
--- basic.c
+++ basic.c
@@ -2,16 +2,43 @@
 #include <avr/interrupt.h>      // definitions for interrupts
 #include <avr/sleep.h>          // definitions for power-down modes
 #include <avr/pgmspace.h>       // definitions or keeping constants in program memory
+#include <stdbool.h>       // true and false!
 
-some crap
+// My Trippy RGB wave:
+//   6 PB1 - R (sink)
+//   7 PB2 - G (sink)
+//   3 PB4 - B (sink)
+//   5 PB0 - IR (sink)
+//   2 PB3 - IR sensor
+
+// Our board:
+//   7 PB2 - R (sink)
+//   5 PB0 - G (sink)
+//   6 PB1 - B (sink)
+//   3 PB4 - IR (source)
+//   2 PB3 - IR sensor
+
+// any variable changed by an interrupt *must* be
+// defined as volatile
+volatile unsigned char bob = 0x00;
+
+ISR(TIM1_OVF_vect) { 
+    PORTB ^= 0b00000010;
+}
 
 int main(void)
 {
-  // Set Port B pins as all outputs
-  DDRB = 0xff;
 
-  // Set all Port B pins as HIGH
-  PORTB = 0xff;
+    OCR1A  = 0x00;              //Set OCR1A 
+    TCCR1 = (1 << CS12)|(0 << CS11)|(1 << CS10); // WGM1=4, prescale at 1024 
+    TIMSK |= (1 << OCIE1A);         //Set bit 6 in TIMSK to enable Timer 1 compare interrupt. 
 
-  return 1;
+    while (true) {
+        // Set output (PB) pins
+        DDRB =  0b00010111;
+
+        // turn on green
+        PORTB = 0b00010011;
+    }
+
 }
