00ca2cbf29d3aafad11e276ffc77c1452ab1152e
Mike Pountney <Mike.Pountney@gmail.com>
2010-10-20 19:34:19 +0100

    god knows what this is doing

--- basic.c
+++ basic.c
@@ -22,23 +22,26 @@
 // defined as volatile
 volatile unsigned char bob = 0x00;
 
-ISR(TIM1_OVF_vect) { 
-    PORTB ^= 0b00000010;
+ISR(TIMER1_OVF_vect) { 
+    PORTB = 0b00010011;
 }
 
 int main(void)
 {
 
-    OCR1A  = 0x00;              //Set OCR1A 
-    TCCR1 = (1 << CS12)|(0 << CS11)|(1 << CS10); // WGM1=4, prescale at 1024 
-    TIMSK |= (1 << OCIE1A);         //Set bit 6 in TIMSK to enable Timer 1 compare interrupt. 
+    TCCR1 = (1 << CS00)|(1 << CS02);
+    TIFR  = 1 << TOV1;
+    TIMSK |= 1 << TOIE1;
+    TCNT1 = 0; // timer counter
+
+    sei();
 
     while (true) {
         // Set output (PB) pins
         DDRB =  0b00010111;
 
-        // turn on green
-        PORTB = 0b00010011;
+        // turn on/off pins in PORTB
+        PORTB =  0b00010100;
     }
 
 }
